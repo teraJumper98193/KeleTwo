8f38ad85b958293a468f92413486b75e06084bf8
teraJumper98193 <tomasz.swider@gmail.com>
2016-08-01 23:29:53 -0400

    Checkpoint 6 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -24,5 +24,35 @@ class Kele
         response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
         @mentor_availability = JSON.parse(response.body)
     end
+    
+
+    def get_messages(*page)
+       if page.empty?
+           response = self.class.get(base_api_endpoint("message_threads"), headers: { "authorization" => @auth_token })
+           @messages = (1..(response["count"]/10 + 1)).map do |i|
+               self.class.get(base_api_endpoint("message_threads?page=#{i}"), headers: { "authorization" => @auth_token })
+
+           end
+              puts @messages
+
+       else
+           response = self.class.get(base_api_endpoint("message_threads?page=#{page.join.to_i}"), headers: { "authorization" => @auth_token })
+           @messages = JSON.parse(response.body)
+
+       end
+   end
+
+   def create_message(user_id, recipient_id, token, subject, stripped)
+       message_data = {body: {user_id: user_id, recipient_id: recipient_id, token: nil, subject: subject, stripped: stripped}, headers: { "authorization" => @auth_token }}
+       self.class.post(base_api_endpoint("messages"), message_data)
+   end
+
+
+
+   private
+
+    def base_api_endpoint(end_point)
+        "https://www.bloc.io/api/v1/#{end_point}"
+    end
 
 end
