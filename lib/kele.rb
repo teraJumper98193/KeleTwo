ef39e59566c550e171515c4961f86fa3481c11bd
teraJumper98193 <tomasz.swider@gmail.com>
2016-08-03 06:14:31 -0400

    Checkpoint 7 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -24,7 +24,12 @@ class Kele
         response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
         @mentor_availability = JSON.parse(response.body)
     end
-    
+
+    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+       submisson_data = {body: {checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id}, headers: { "authorization" => @auth_token }}
+       self.class.post(base_api_endpoint("checkpoint_submissions"), submisson_data)
+    end
+
 
     def get_messages(*page)
        if page.empty?
